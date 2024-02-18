33d563d2962d9c089ed4d7496f0e1e682aafbf02
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-02-18 21:58:01 +0530

    password hashing and signup route

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const userRoute = require("./routes/user.js");
+const signupRoute = require("./routes/auth.js");
 
-dotenv.config()
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,6 +16,11 @@ mongoose
     console.log(err);
   });
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server running at Port 3000");
 });
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", signupRoute);
