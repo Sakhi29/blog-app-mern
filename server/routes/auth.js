1f0a8e67a987f2b73995ba1490b5874b2e0c5d82
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-02-23 14:32:09 +0530

    added google Oauth using firebase

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const signup = require("../controllers/auth");
 const signin = require("../controllers/auth");
+const google = require("../controllers/auth");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/google", google);
 
 module.exports = router;
