7ebe525d7779f1095c1d927cff82a7471fbcb025
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-03-26 19:15:53 +0530

    create getposts api route

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const { verifyToken } = require("../utils/verifyUser");
-const { create } = require("../controllers/post");
+const { create, getposts } = require("../controllers/post");
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/getposts", getposts);
 
 module.exports = router;
