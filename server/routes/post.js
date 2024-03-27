1787cd18f64e3d656a4e884988ed61acf745d4d5
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-03-27 18:24:35 +0530

    delete post functionality added

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { verifyToken } = require("../utils/verifyUser");
-const { create, getposts } = require("../controllers/post");
+const { create, getposts, deletepost } = require("../controllers/post");
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 
 module.exports = router;
