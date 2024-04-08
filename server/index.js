41916a49e928884d14973a5080391cd137f45bba
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-04-08 18:21:48 +0530

    comment functionality added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const userRoute = require("./routes/user.js");
 const authRoute = require("./routes/auth.js");
 const postRoute = require("./routes/post.js");
+const commentRoute = require("./routes/comment.js");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
+app.use("/api/comment", commentRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
