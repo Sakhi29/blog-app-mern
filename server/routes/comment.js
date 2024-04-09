ef7e41ea00cddbd2b9f9c53999f30c0b19812469
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-04-09 21:49:25 +0530

    like,edit,delete functionality added on comments

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { verifyToken } = require("../utils/verifyUser");
-const { createComment, getPostComments } = require("../controllers/comment");
+const {
+  createComment,
+  getPostComments,
+  likeComment,
+  editComment,
+  deleteComment
+} = require("../controllers/comment");
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 module.exports = router;
